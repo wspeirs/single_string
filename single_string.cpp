0f84d6bf26480b96af4d79be0331702843417b6c
William Speirs <bill.speirs@gmail.com>
2015-04-01 17:26:00 -0400

    Added some operators

--- single_string.cpp
+++ single_string.cpp
@@ -1,6 +1,11 @@
 #include "single_string.h"
 
 single_string::string_table_t single_string::string_table;
+const single_string single_string::EMPTY_STRING("");
+
+single_string::single_string() {
+    this->str_ptr = EMPTY_STRING.str_ptr;
+}
 
 single_string::single_string(const std::string &str) {
     auto it = string_table.find(str);
@@ -17,34 +22,6 @@ void single_string::free_all() {
     for(auto s : string_table) {
         delete s.second;
     }
-}
-
-inline const char* single_string::c_str() const {
-    return str_ptr->c_str();
-}
-
-inline char& single_string::operator[](std::size_t pos ) {
-    return str_ptr->operator[](pos);
-}
-
-inline const char& single_string::operator[](std::size_t pos) const {
-    return str_ptr->operator[](pos);
-}
-
-inline int single_string::compare(const std::string& str) const {
-    return str_ptr->compare(str);
-}
-
-inline int single_string::compare(const char* str) const {
-    return str_ptr->compare(str);
-}
-
-inline int single_string::compare(const single_string& str) const {
-    return str_ptr->compare(*(str.str_ptr));
-}
-
-bool operator==(const single_string& lhs, const single_string& rhs) {
-    return lhs.str_ptr == rhs.str_ptr; // only need to check pointers
 }
 
 namespace std {
--- single_string.h
+++ single_string.h
@@ -8,31 +8,72 @@ class single_string {
 private:
     typedef std::unordered_map<std::string, std::string*> string_table_t;
     static string_table_t string_table;
+    static const single_string EMPTY_STRING;
 
     std::string *str_ptr;
 
     friend class std::hash<single_string>; // allows hash to access str_ptr
     friend bool operator==(const single_string& lhs, const single_string& rhs);
+    friend bool operator==(const single_string &lhs, const char *rhs);
+    friend bool operator!=(const single_string& lhs, const single_string& rhs);
+    friend bool operator!=(const single_string &lhs, const char *rhs);
 
 public:
+    single_string();
+
     single_string(const std::string &str);
 
     static void free_all();
 
-    inline const char* c_str() const;
+    inline std::string& to_string() const {
+        return *str_ptr;
+    }
 
-    inline char& operator[](std::size_t pos );
+    inline const char* c_str() const {
+        return str_ptr->c_str();
+    }
 
-    inline const char& operator[](std::size_t pos) const;
+    inline char& operator[](std::size_t pos ) {
+        return str_ptr->operator[](pos);
+    }
 
-    inline int compare(const std::string& str) const;
+    inline const char& operator[](std::size_t pos) const {
+        return str_ptr->operator[](pos);
+    }
 
-    inline int compare(const char* str) const;
+    inline int compare(const std::string& str) const {
+        return str_ptr->compare(str);
+    }
 
-    inline int compare(const single_string& str) const;
+    inline int compare(const char* str) const {
+        return str_ptr->compare(str);
+    }
+
+    inline int compare(const single_string& str) const {
+        return str_ptr->compare(*(str.str_ptr));
+    }
 };
 
-bool operator==(const single_string& lhs, const single_string& rhs);
+inline bool operator==(const single_string& lhs, const single_string& rhs) {
+    return lhs.str_ptr == rhs.str_ptr; // only need to check pointers
+}
+
+inline bool operator==(const single_string &lhs, const char *rhs) {
+    return lhs.str_ptr == single_string(rhs).str_ptr;
+}
+
+inline bool operator!=(const single_string& lhs, const single_string& rhs) {
+    return lhs.str_ptr != rhs.str_ptr; // only need to check pointers
+}
+
+inline bool operator!=(const single_string &lhs, const char *rhs) {
+    return lhs.str_ptr != single_string(rhs).str_ptr;
+}
+
+inline std::string operator+(const std::string &lhs, const single_string &rhs) {
+    return lhs + rhs.to_string();
+}
+
 
 namespace std {
     template <>
